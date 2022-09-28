ec69634a774c16a3482d6da562900d5edd522e97
awaliya123 <113803711+awaliya123@users.noreply.github.com>
2022-09-28 22:51:41 +0700

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -218,7 +218,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAx0CZIiVngABBW4-YzRtYnF6OI81aGiov9hG4rIbRwIAAq4GAAIjjsFVC6wG-IXP_7kqBA"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
